631ef9c4ab23be5b02576f781008d165b98a0210
gagan-uk <steamgaganuk@gmail.com>
2025-12-07 00:39:46 +0530

    added icon

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 
 int main(void){
   
-  const int screenW = 900;
-  const int screenH = 600;
-  const int screenFont = 20;
+  const int screenW {900};
+  const int screenH {600};
+  const int screenFont {20};
+  const char* windowTitle {"ImvI"};
+  const char* iconPath{"ico.png"};
   
-  InitWindow(screenW, screenH, "Imvi");
-  SetWindowState(FLAG_WINDOW_RESIZEABLE);
+  InitWindow(screenW, screenH, windowTitle);
+  SetWindowState(FLAG_WINDOW_RESIZABLE);
+  
+  
+  Image icon {LoadImage(iconPath)};
+  SetWindowIcon(icon);
+  UnloadImage(icon);
 
-  const char* text = "Image here";
-  int textWidth = MeasureText(text, screenFont);
-  int screenC = (screenW - textWidth) / 2;  
-  int screenCY = (screenH - screenFont) / 2;
+  const char* text {"Image here"};
+  int textWidth { MeasureText(text, screenFont)};
+  int screenC { (screenW - textWidth) / 2};  
+  int screenCY { (screenH - screenFont) / 2};
   
   while (!WindowShouldClose()){
 
